719e38ff0e0ba82b122445011e0728f1c2b9c566
Helen Kapatsa <kapatsahelen@gmail.com>
2020-01-25 08:01:11 +0300

    FIdami created: - [✓] Develop an import-based chart plotters - [✓] Customize stylesheet

---
+++ chart_plotter/repository.h
@@ -0,0 +1,11 @@
+#ifndef REPOSITORY_H
+#define REPOSITORY_H
+
+
+class repository
+{
+public:
+    repository();
+};
+
+#endif // REPOSITORY_H
---
+++ fidami/router.h
@@ -0,0 +1,11 @@
+#ifndef ROUTER_H
+#define ROUTER_H
+
+
+class router
+{
+public:
+    router();
+};
+
+#endif // ROUTER_H
